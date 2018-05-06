d7163d992bb9317c8d16ad950887522bcf629da1
Christian Passold <passold.christian@hotmail.com>
2018-05-06 17:17:00 -0300

    updates

--- src/routes.js
+++ src/routes.js
@@ -4,7 +4,7 @@ import {GridColumn} from 'semantic-ui-react';
 
 // COMPONENTS
 import App from './app'
-import Home from "./pages/home/home";
+import Home from "./containers/home/home";
 
 const NotFound = () => <GridColumn><h1>404.. This page is not found!</h1></GridColumn>
 
