b6645a8720a7be4c3dfdb491aac066dc52e73336
christian.passold <christian.passold@hbsis.com.br>
2018-05-09 13:33:33 -0300

    Ajustes no layout

--- src/components/fields/checkbox/CheckboxCheckGreen.js
+++ src/components/fields/checkbox/CheckboxCheckGreen.js
@@ -1,9 +1,25 @@
 import React from 'react'
 
-const CheckboxCheckGreen = ({label, onChange, ...rest}) => <div className="pretty p-default p-round p-smooth p-plain">
+// const CheckboxCheckGreen = ({label, onChange, ...rest}) => <div className="pretty p-default p-round p-smooth p-plain">
+//     <input {...rest} type="checkbox" onChange={(e) => !!onChange && onChange(e.target.checked)}/>
+//     <div className="state p-success-o">
+//         <label>{label}</label>
+//     </div>
+// </div>
+
+const style = {
+    checked: {
+        textDecoration: 'line-through',
+        opacity: 0.6
+    }
+}
+//TODO: fazer componente controlado e colocar um icone de check
+const CheckboxCheckGreen = ({label, onChange, defaultChecked, ...rest}) => <div
+    className="pretty p-icon p-round p-jelly">
     <input {...rest} type="checkbox" onChange={(e) => !!onChange && onChange(e.target.checked)}/>
-    <div className="state p-success-o">
-        <label>{label}</label>
+    <div className="state p-primary">
+        <i className="icon mdi mdi-check"/>
+        <label style={!!defaultChecked ? style.checked : {}}>{label}</label>
     </div>
 </div>
 
--- src/components/lists/item/ShoppingListItem.js
+++ src/components/lists/item/ShoppingListItem.js
@@ -1,17 +1,17 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import {Grid, GridColumn, GridRow, Icon, Segment} from 'semantic-ui-react'
+import {Grid, GridColumn, GridRow, Segment} from 'semantic-ui-react'
 import CheckboxCheckGreen from "components/fields/checkbox/CheckboxCheckGreen";
 
 const ShoppingListItem = ({data, isDone = false, onChange}) => <Segment>
     <Grid>
         <GridRow>
-            <GridColumn verticalAlign width={15}>
+            <GridColumn verticalAlign={'middle'} width={14}>
                 <CheckboxCheckGreen label={data} defaultChecked={isDone} onChange={onChange}/>
             </GridColumn>
-            <GridColumn verticalAlign textAlign={'right'}>
-                <Icon size={'small'} floated={'right'} name='delete'/>
-            </GridColumn>
+            {/*<GridColumn verticalAlign={'middle'} textAlign={'right'}>*/}
+            {/*<Icon size={'large'} floated={'right'} name='delete'/>*/}
+            {/*</GridColumn>*/}
         </GridRow>
     </Grid>
 </Segment>
