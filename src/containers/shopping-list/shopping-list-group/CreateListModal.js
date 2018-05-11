45ac21b128035eb0172898ac8ef17e5e22fb5774
Christian Passold <passold.christian@hotmail.com>
2018-05-11 20:33:18 -0300

    WIP

--- src/containers/shopping-list/shopping-list-group/CreateListModal.js
+++ src/containers/shopping-list/shopping-list-group/CreateListModal.js
@@ -2,13 +2,19 @@ import React, {Component} from 'react'
 import PropTypes from 'prop-types'
 import {Button, Input, Modal} from 'semantic-ui-react'
 
+const style = {
+    input: {
+        color: 'rgba(255, 255, 255, 0.87) !important',
+    }
+}//todo: mudar cor para branco
+
 export default class CreateListModal extends Component {
 
     render() {
         return <Modal basic size={'small'} dimmer={true} open={this.props.open} onClose={this.props.onClose}>
-            <Modal.Header>Nova lista</Modal.Header>
             <Modal.Content>
-                <Input/>
+                <Input autoFocus transparent fluid placeholder={'Ex.: Churras'}
+                       size={'massive'} style={style.input}/>
             </Modal.Content>
             <Modal.Actions>
                 <Button inverted color={'red'} content={'NOPE'} onClick={this.props.onClose}/>
